462ef18a97642d523125b2e2b71fd46aced3487f
Andrew Morgan <andrew.morgan@e.org>
2021-11-29 10:53:51 +0000

    Added ICartRepository definition

---
+++ DominicsPizza.Repositories/Interfaces/ICartRepository.cs
@@ -0,0 +1,10 @@
+﻿using DominicsPizza.Entities;
+using System;
+
+namespace DominicsPizza.Repositories.Interfaces
+{
+    public interface ICartRepository : IRepository<Cart>
+    {
+        Cart GetCart(Guid CartId);
+    }
+}
