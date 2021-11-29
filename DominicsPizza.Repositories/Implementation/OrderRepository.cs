4d3a6724ae89e656f245bf5081ae36d227759e24
Andrew Morgan <andrew.morgan@e.org>
2021-11-29 16:56:29 +0000

    Added OrderRepository Definition and implementation

---
+++ DominicsPizza.Repositories/Implementation/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using DominicsPizza.Entities;
+using DominicsPizza.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DominicsPizza.Repositories.Implementation
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        private AppDbContext appContext => _dbContext as AppDbContext;
+
+        public OrderRepository(DbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public IEnumerable<Order> GetUserOrders(int UserId)
+        {
+            return appContext.Orders
+                .Include(o => o.OrderItems)
+                .Where(x => x.UserId == UserId).ToList();
+        }
+    }
+}
---
+++ DominicsPizza.Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using DominicsPizza.Entities;
+using System.Collections.Generic;
+
+namespace DominicsPizza.Repositories.Interfaces
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<Order> GetUserOrders(int UserId);
+    }
+}
