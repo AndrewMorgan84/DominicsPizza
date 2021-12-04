be859f18520ad7d1ad803cd4a4ce2efbf54db01c
Andrew <andrew.morgan@e.org>
2021-12-04 16:57:31 +0000

    Added Authentication Service interface and implementation

---
+++ DominicsPizza.Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using DominicsPizza.Entities;
+using System.Threading.Tasks;
+
+namespace DominicsPizza.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        bool CreateUser(User user, string Password);
+
+        Task<bool> SignOut();
+
+        User AuthenticateUser(string Username, string Password);
+
+        User GetUser(string Username);
+    }
+}
