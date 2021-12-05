f88977fe2063264e994927f5deb7a61c05ef27c0
Andrew <andrew.morgan@e.org>
2021-12-05 14:55:49 +0000

    Updated Account controller with login and signup actions

--- DominicsPizza.WebUI/Controllers/AccountController.cs
+++ DominicsPizza.WebUI/Controllers/AccountController.cs
@@ -1,11 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DominicsPizza.Entities;
+using DominicsPizza.Services.Interfaces;
+using DominicsPizza.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DominicsPizza.WebUI.Controllers
 {
     public class AccountController : Controller
     {
+        IAuthenticationService _authService;
+        public AccountController(IAuthenticationService authService)
+        {
+            _authService = authService;
+        }
+
+        public IActionResult Login() 
+        {
+            return View();
+        }
+
         public IActionResult SignUp()
         {
+            return View();
+        }
+
+        public IActionResult SignUp(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User()
+                {
+                    Name = model.Name,
+                    UserName = model.Email,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber
+                };
+
+                bool result = _authService.CreateUser(user, model.Password);
+                if (result)
+                {
+                    RedirectToAction("Login");
+                }
+            }
             return View();
         }
     }
