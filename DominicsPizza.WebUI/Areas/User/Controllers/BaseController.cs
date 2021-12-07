6d9b8b99ca5e1e232a516dd1cf4709f0c346e674
Andrew <andrew.morgan@e.org>
2021-12-07 12:35:09 +0000

    Added User Base Controller

---
+++ DominicsPizza.WebUI/Areas/User/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DominicsPizza.WebUI.Areas.User.Controllers
+{
+    [Area("User")]
+    public class BaseController : Controller
+    {
+        
+    }
+}
--- DominicsPizza.WebUI/Areas/User/Controllers/DashboardController.cs
+++ DominicsPizza.WebUI/Areas/User/Controllers/DashboardController.cs
@@ -2,8 +2,7 @@
 
 namespace DominicsPizza.WebUI.Areas.User.Controllers
 {
-    [Area("User")]
-    public class DashboardController : Controller
+    public class DashboardController : BaseController
     {
         public IActionResult Index()
         {
