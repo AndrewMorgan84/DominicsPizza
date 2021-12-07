87d9faa9500f996832ae0cd623045d2359782827
Andrew <andrew.morgan@e.org>
2021-12-07 12:33:44 +0000

    Added Admin Base Controller

--- DominicsPizza.WebUI/Areas/Admin/Controllers/DashboardController.cs
+++ DominicsPizza.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -2,8 +2,7 @@
 
 namespace DominicsPizza.WebUI.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    public class DashboardController : Controller
+    public class DashboardController : BaseController
     {
         public IActionResult Index()
         {
