ae6a64cbe2deb66ff0bb5e2a0ff557b0a3cdfdf9
Andrew <andrew.morgan@e.org>
2021-12-09 12:03:55 +0000

    Added Basic Item Controller Added and implemented Catalog Service

---
+++ DominicsPizza.Services/Interfaces/ICatalogService.cs
@@ -0,0 +1,26 @@
+﻿using DominicsPizza.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominicsPizza.Services.Interfaces
+{
+    public interface ICatalogService
+    {
+        IEnumerable<Category> GetCategories();
+
+        IEnumerable<ItemType> GetItemTypes();
+
+        IEnumerable<Item> GetItems();
+
+        Item GetItem(int id);
+
+        void AddItem(Item item);
+
+        void UpdateItem(Item item);
+
+        void DeleteItem(int id);
+    }
+}
