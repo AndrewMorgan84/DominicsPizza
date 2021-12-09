a95253de7223e49ceed2858b8001dd68f30f6376
Andrew <andrew.morgan@e.org>
2021-12-09 15:23:06 +0000

    Added Create functionality to the item controller Added a Create View

--- DominicsPizza.WebUI/Areas/Admin/Controllers/ItemController.cs
+++ DominicsPizza.WebUI/Areas/Admin/Controllers/ItemController.cs
@@ -1,19 +1,20 @@
-﻿using DominicsPizza.Services.Interfaces;
+﻿using DominicsPizza.Entities;
+using DominicsPizza.Services.Interfaces;
+using DominicsPizza.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DominicsPizza.WebUI.Areas.Admin.Controllers
 {
     public class ItemController : BaseController
     {
         ICatalogService _catalogService;
+        IFileHelper _fileHelper;
 
-        public ItemController(ICatalogService catalogService)
+        public ItemController(ICatalogService catalogService, IFileHelper fileHelper)
         {
             _catalogService = catalogService;
+            _fileHelper = fileHelper;
         }
         public IActionResult Index()
         {
@@ -23,6 +24,36 @@ namespace DominicsPizza.WebUI.Areas.Admin.Controllers
 
         public IActionResult Create()
         {
+            ViewBag.Categories = _catalogService.GetCategories();
+            ViewBag.ItemTypes = _catalogService.GetItemTypes();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ItemModel model)
+        {
+            try
+            {
+                model.ImageUrl = _fileHelper.UploadFile(model.File);
+                Item data = new Item()
+                {
+                    Name = model.Name,
+                    UnitPrice = model.UnitPrice,
+                    CategoryId = model.CategoryId,
+                    ItemTypeId = model.ItemTypeId,
+                    Description = model.Description,
+                    ImageUrl = model.ImageUrl
+                };
+
+                _catalogService.AddItem(data);
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+            }
+            ViewBag.Categories = _catalogService.GetCategories();
+            ViewBag.ItemTypes = _catalogService.GetItemTypes();
             return View();
         }
     }
