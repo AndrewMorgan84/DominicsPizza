190e4d5a634d3195b6e4868b0ae2b851762539ca
Andrew <andrew.morgan@e.org>
2021-12-10 11:48:40 +0000

    Added Item Edit functionality for Admin user

--- DominicsPizza.WebUI/Areas/Admin/Controllers/ItemController.cs
+++ DominicsPizza.WebUI/Areas/Admin/Controllers/ItemController.cs
@@ -56,5 +56,59 @@ namespace DominicsPizza.WebUI.Areas.Admin.Controllers
             ViewBag.ItemTypes = _catalogService.GetItemTypes();
             return View();
         }
+
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Categories = _catalogService.GetCategories();
+            ViewBag.ItemTypes = _catalogService.GetItemTypes();
+            Item data = _catalogService.GetItem(id);
+            ItemModel model = new ItemModel()
+            {
+                Id = data.Id,
+                Name = data.Name,
+                UnitPrice = data.UnitPrice,
+                CategoryId = data.CategoryId,
+                ItemTypeId = data.ItemTypeId,
+                Description = data.Description,
+                ImageUrl = data.ImageUrl
+            };
+
+            return View("Create", model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ItemModel model)
+        {
+            try
+            {
+                if(model.File != null)
+                {
+                    _fileHelper.DeleteFile(model.ImageUrl);
+                    model.ImageUrl = _fileHelper.UploadFile(model.File);
+                }
+
+                Item data = new Item
+                {
+                    Id = model.Id,
+                    Name = model.Name,
+                    UnitPrice = model.UnitPrice,
+                    CategoryId = model.CategoryId,
+                    ItemTypeId = model.ItemTypeId,
+                    Description = model.Description,
+                    ImageUrl = model.ImageUrl
+                };
+
+                _catalogService.UpdateItem(data);
+                return RedirectToAction("Index");
+            }
+            catch(Exception)
+            {
+
+            }
+
+            ViewBag.Categories = _catalogService.GetCategories();
+            ViewBag.ItemTypes = _catalogService.GetItemTypes();
+            return View("Create", model);
+        }
     }
 }
