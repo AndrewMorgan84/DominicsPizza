fa5338b915092ce17ddc961378762785bc161947
Andrew <andrew.morgan@e.org>
2021-12-15 19:05:25 +0000

    Added CartController and index

--- DominicsPizza.Services/Interfaces/ICartService.cs
+++ DominicsPizza.Services/Interfaces/ICartService.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace DominicsPizza.Services.Interfaces
 {
-    interface ICartService
+    public interface ICartService
     {
         int GetCartCount(Guid cartId);
 
---
+++ DominicsPizza.WebUI/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using DominicsPizza.Repositories.Models;
+using DominicsPizza.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DominicsPizza.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        Guid CartId
+        {
+            get
+            {
+                Guid Id;
+                string CId = Request.Cookies["CId"];
+                if (string.IsNullOrEmpty(CId))
+                {
+                    Id = Guid.NewGuid();
+                    Response.Cookies.Append("CId", Id.ToString());
+                }
+                else
+                {
+                    Id = Guid.Parse(CId);
+                }
+                return Id;
+            }
+        }
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IActionResult Index()
+        {
+            CartModel cart = _cartService.GetCartDetails(CartId);
+            return View(cart);
+        }
+    }
+}
