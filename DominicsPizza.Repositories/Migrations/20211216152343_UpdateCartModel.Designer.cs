6a336dd2fc7ab2763fcc727c8abe0aa34ee5cf72
Andrew <andrew.morgan@e.org>
2021-12-16 15:27:09 +0000

    Added Migrations to fix broken cart model

--- DominicsPizza.Repositories/Migrations/20211216152343_UpdateCartModel.Designer.cs
+++ DominicsPizza.Repositories/Migrations/20211216152343_UpdateCartModel.Designer.cs
@@ -4,14 +4,16 @@ using DominicsPizza.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DominicsPizza.Repositories.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211216152343_UpdateCartModel")]
+    partial class UpdateCartModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DominicsPizza.Repositories/Migrations/20211216152343_UpdateCartModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DominicsPizza.Repositories.Migrations
+{
+    public partial class UpdateCartModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "guid",
+                table: "Carts",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Carts",
+                newName: "guid");
+        }
+    }
+}
